ccf975ca6a1242b06155f9389c0a71d319ee2bb4
Theodorus Clarence <theodorusclarence@gmail.com>
2023-12-21 10:14:55 +0700

    feat: add github sync feature flag

--- src/lib/replicache/backend-mutators.server.ts
+++ src/lib/replicache/backend-mutators.server.ts
@@ -4,12 +4,24 @@ import { MutationName } from '@/lib/replicache/mutators';
 
 import { TodoCreateArgs, TodoDeleteArgs } from '@/types/todo';
 
+const GITHUB_SYNC_ENABLED = process.env.NEXT_PUBLIC_SYNC_WITH_GITHUB === 'true';
+
 export const backendMutators = {
   todoCreate: async (
     tx: TransactionalPrismaClient,
     args: TodoCreateArgs,
     version: number
   ) => {
+    if (!GITHUB_SYNC_ENABLED) {
+      await tx.todo.create({
+        data: {
+          ...args,
+          version,
+        },
+      });
+      return;
+    }
+
     const issue = await octokit.rest.issues.create({
       owner: process.env.NEXT_PUBLIC_GITHUB_OWNER ?? 'theodorusclarence',
       repo: process.env.NEXT_PUBLIC_GITHUB_REPO ?? 'dimension-dump',
@@ -39,6 +51,19 @@ export const backendMutators = {
     args: TodoDeleteArgs,
     version: number
   ) => {
+    if (!GITHUB_SYNC_ENABLED) {
+      await tx.todo.update({
+        where: {
+          id: args.id,
+        },
+        data: {
+          isDeleted: true,
+          version,
+        },
+      });
+      return;
+    }
+
     const updatedTodo = await tx.todo.update({
       where: {
         id: args.id,
