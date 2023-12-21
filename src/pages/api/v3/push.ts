95bb1c2db62176b9b4cef8dd08cfe2f2c66f7f26
Aaron Boodman <aaron@aaronboodman.com>
2023-12-20 15:15:17 -1000

    fix: optimistic mutations weren't getting cleared

--- src/pages/api/v3/push.ts
+++ src/pages/api/v3/push.ts
@@ -153,6 +153,7 @@ export default async function handler(
                 },
                 data: {
                   lastMutationId,
+                  version: nextVersion,
                 },
               });
             }
