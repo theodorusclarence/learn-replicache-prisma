ac1d38a25715cb81d0998fd570f340725e6a0c9d
Theodorus Clarence <theodorusclarence@gmail.com>
2023-12-27 16:49:14 +0700

    fix: project is not instant

--- src/lib/client/mutators/todo.mutator.ts
+++ src/lib/client/mutators/todo.mutator.ts
@@ -1,10 +1,19 @@
 import { TodoMutators } from '@/models/mutator/todo.model';
+import { removeUndefinedFromObject } from '@/utils/client/helpers';
 
 export const clientTodoMutators: (spaceId: string) => TodoMutators<'client'> = (
   spaceId
 ) => ({
   async todoCreate(tx, args) {
-    await tx.set(`${spaceId}/todo/${args.id}`, args);
+    const project = await tx.get(`${spaceId}/project/${args.projectId}`);
+
+    await tx.set(
+      `${spaceId}/todo/${args.id}`,
+      removeUndefinedFromObject({
+        ...args,
+        project,
+      })
+    );
   },
   async todoDelete(tx, args) {
     await tx.del(`${spaceId}/todo/${args.id}`);
