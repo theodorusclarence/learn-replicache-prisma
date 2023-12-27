f4efa47544803dc299a25f85d271c8ab8eacfeda
Theodorus Clarence <theodorusclarence@gmail.com>
2023-12-27 20:00:49 +0700

    fix: idb key doesn't work

--- src/models/idb-key.model.ts
+++ src/models/idb-key.model.ts
@@ -1,6 +1,6 @@
 export const IDB_KEY = {
-  TODO: ({ spaceId, id }: { spaceId: string | null; id?: string }) =>
-    `${spaceId}/${id}`,
-  PROJECT: ({ spaceId, id }: { spaceId: string | null; id?: string }) =>
+  TODO: ({ spaceId, id = '' }: { spaceId: string | null; id?: string }) =>
+    `${spaceId}/todo/${id}`,
+  PROJECT: ({ spaceId, id = '' }: { spaceId: string | null; id?: string }) =>
     `${spaceId}/project/${id}`,
 };
